8cb176d603d564b124ac0d5f21a361d1331eabdf
Danny Rodriguez <danny007111@hotmail.com>
2022-06-05 16:41:14 -0500

    Making changes

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Filter({ onCategoryChange }) {
+
+  const[searchTerm, setSearchTerm] = useState("testing")
+
+
+  function onSearchChange (event){
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" name="search" placeholder="Search..." onChange={onSearchChange}/>
+
       <select name="filter" onChange={onCategoryChange}>
+
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
         <option value="Dessert">Dessert</option>
+
       </select>
     </div>
   );
